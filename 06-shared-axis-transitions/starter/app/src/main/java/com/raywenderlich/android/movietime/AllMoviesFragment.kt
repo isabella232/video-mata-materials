b26da4bb0be7881d3e42ce742b2feb273a2723fb
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2021-12-14 05:06:49 +0530

    Add projects for episode 6

---
+++ 06-shared-axis-transitions/starter/app/src/main/java/com/raywenderlich/android/movietime/AllMoviesFragment.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.movietime
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.doOnPreDraw
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialFadeThrough
+import com.raywenderlich.android.movietime.databinding.FragmentMoviesBinding
+
+class AllMoviesFragment: Fragment() {
+
+  private var _binding: FragmentMoviesBinding? = null
+  private val binding get() = _binding!!
+
+  private lateinit var movieAdapter: MovieAdapter
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enterTransition = MaterialFadeThrough()
+  }
+
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+    movieAdapter = MovieAdapter(object: MovieAdapter.MovieListener {
+      override fun onClick(
+          movieId: Long,
+          posterImageView: ImageView,
+          card: View
+      ) {
+        val extras = FragmentNavigatorExtras(
+            posterImageView to getString(R.string.poster_transition_name_id, movieId),
+            card to  getString(R.string.card_transition_name_id, movieId)
+        )
+        findNavController().navigate(
+            AllMoviesFragmentDirections.actionAllMoviesFragmentToMovieDetailsFragment(
+                movieId
+            ),
+            extras
+        )
+      }
+    })
+    binding.moviesRecyclerview.adapter = movieAdapter
+
+    movieAdapter.setMovies(MovieRepository.allMovies)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    postponeEnterTransition()
+    view.doOnPreDraw { startPostponedEnterTransition() }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+}
