8c25a38c5dcc38faf24ad293ab10a81e04d847e5
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2021-12-14 02:29:01 +0530

    Add projects for chapter 2 and 3

---
+++ 03-fadethrough-transitions-and-delaying-transitions/final/app/src/main/java/com/raywenderlich/android/movietime/MovieDetailsFragment.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.movietime
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.transition.TransitionManager
+import com.bumptech.glide.Glide
+import com.google.android.material.transition.MaterialFade
+import com.raywenderlich.android.movietime.databinding.FragmentMovieDetailsBinding
+
+class MovieDetailsFragment: Fragment() {
+
+  private var _binding: FragmentMovieDetailsBinding? = null
+  private val binding get() = _binding!!
+
+  private val args: MovieDetailsFragmentArgs by navArgs()
+  private lateinit var movie: MovieModel
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    movie = MovieRepository.getMovie(args.movieId)
+  }
+
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+
+    with(binding) {
+      Glide.with(moviePosterImageview)
+          .load(movie.posterLink)
+          .into(moviePosterImageview)
+      movieNameTextview.text = movie.name
+      movieDescriptionTextview.text = movie.summary
+      ratingsTextview.text = movie.ratings.toString()
+      showRatingsButton.setOnClickListener {
+        TransitionManager.beginDelayedTransition(root, MaterialFade())
+        if (ratingsTextview.isVisible) {
+          ratingsTextview.isInvisible = true
+          showRatingsButton.text = resources.getString(R.string.show_ratings)
+        } else {
+          ratingsTextview.isInvisible = false
+          showRatingsButton.text = resources.getString(R.string.hide_ratings)
+        }
+      }
+
+      moviePosterImageview.setOnClickListener {
+        findNavController().navigate(
+            MovieDetailsFragmentDirections.actionMovieDetailsFragmentToPosterFragment(
+                movie.id
+            )
+        )
+      }
+    }
+    return binding.root
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+}
